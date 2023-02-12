bd37d1be0d726266438910c1ac9a4a342fee43e7
FrankAlbella <FrankAlbella@gmail.com>
2023-02-11 20:49:10 -0500

    Add basic flutter_bluetooth_serial implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,34 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+
+import 'BluetoothDeviceListEntry.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 Widget _buildPopupDialog(BuildContext context) {
-  return new AlertDialog(
+  return AlertDialog(
     title: const Text('Popup example'),
-    content: new Column(
+    content: Column(
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
+      children: const <Widget>[
         Text("Hello"),
       ],
     ),
     actions: <Widget>[
-      new TextButton(
+      TextButton(
         onPressed: () {
           Navigator.of(context).pop();
         },
         child: const Text('Cancel'),
       ),
-      new TextButton(
+      TextButton(
         onPressed: () {
           Navigator.of(context).pop();
         },
@@ -368,22 +375,88 @@ class PairingPage extends StatefulWidget {
 
   final String title;
 
+
   @override
   State<PairingPage> createState() => _PairingPage();
 }
 
 class _PairingPage extends State<PairingPage> {
-  int _counter = 0;
+  StreamSubscription<BluetoothDiscoveryResult>? _streamSubscription;
+  List<BluetoothDiscoveryResult> results = List<BluetoothDiscoveryResult>.empty(growable: true);
+  bool isDiscovering = true;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+
+    if (isDiscovering) {
+      _startDiscovery();
+      requestDiscoverable(120);
+    }
+  }
+
+  @override
+  void dispose() {
+    // Avoid memory leak (`setState` after dispose) and cancel discovery
+    _streamSubscription?.cancel();
+
+    super.dispose();
+  }
+
+  void _restartDiscovery() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      results.clear();
+      isDiscovering = true;
     });
+
+    _startDiscovery();
+  }
+
+  Future<int?> requestDiscoverable(int seconds) async {
+    return FlutterBluetoothSerial.instance.requestDiscoverable(seconds);
+  }
+
+  void _startDiscovery() {
+    _streamSubscription =
+        FlutterBluetoothSerial.instance.startDiscovery().listen((r) {
+          setState(() {
+            final existingIndex = results.indexWhere(
+                    (element) => element.device.address == r.device.address);
+            if (existingIndex >= 0) {
+              results[existingIndex] = r;
+            } else {
+              results.add(r);
+            }
+          });
+        });
+
+    _streamSubscription!.onDone(() {
+      setState(() {
+        isDiscovering = false;
+      });
+    });
+  }
+
+  Future<void> connectToAddress(String? address) async {
+    try {
+      BluetoothConnection connection = await BluetoothConnection.toAddress(address);
+      print('Connected to the device');
+
+      connection.input?.listen((Uint8List data) {
+        print('Data incoming: ${ascii.decode(data)}');
+        connection.output.add(data); // Sending data
+
+        if (ascii.decode(data).contains('!')) {
+          connection.finish(); // Closing connection
+          print('Disconnecting by local host');
+        }
+      }).onDone(() {
+        print('Disconnected by remote request');
+      });
+    }
+    catch (exception) {
+      print('Cannot connect, exception occured');
+    }
   }
 
   @override
@@ -396,30 +469,77 @@ class _PairingPage extends State<PairingPage> {
         backgroundColor: Colors.white,
         automaticallyImplyLeading: false,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            TextButton.icon(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.bluetooth,
-                ),
-                label: const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text("Pair with Parter"))),
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: results.length,
+        itemBuilder: (BuildContext context, index) {
+          BluetoothDiscoveryResult result = results[index];
+          final device = result.device;
+          final address = device.address;
+          return BluetoothDeviceListEntry(
+            device: device,
+            rssi: result.rssi,
+            onTap: () {
+              Navigator.of(context).pop(result.device);
+            },
+            onLongPress: () async {
+              try {
+                bool bonded = false;
+                if (device.isBonded) {
+                  print('Unbonding from ${device.address}...');
+                  await FlutterBluetoothSerial.instance
+                      .removeDeviceBondWithAddress(address);
+                  print('Unbonding from ${device.address} has succeed');
+                } else {
+                  print('Bonding with ${device.address}...');
+                  bonded = (await FlutterBluetoothSerial.instance
+                      .bondDeviceAtAddress(address))!;
+                  print(
+                      'Bonding with ${device.address} has ${bonded ? 'succeed' : 'failed'}.');
+                }
+                setState(() {
+                  results[results.indexOf(result)] = BluetoothDiscoveryResult(
+                      device: BluetoothDevice(
+                        name: device.name ?? '',
+                        address: address,
+                        type: device.type,
+                        bondState: bonded
+                            ? BluetoothBondState.bonded
+                            : BluetoothBondState.none,
+                      ),
+                      rssi: result.rssi);
+                });
+              } catch (ex) {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('Error occurred while bonding'),
+                      content: Text(ex.toString()),
+                      actions: <Widget>[
+                        TextButton(
+                          child: const Text("Close"),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              }
+            },
+          );
+        },
       ),
       persistentFooterButtons: [
         IconButton(
-          icon: Icon(Icons.arrow_back_rounded),
+          icon: const Icon(Icons.arrow_back_rounded),
           onPressed: () {
             Navigator.pop(context);
           },
           alignment: Alignment.bottomLeft,
         ),
-        SizedBox(width: 100),
+        const SizedBox(width: 100),
       ],
       persistentFooterAlignment: AlignmentDirectional.bottomStart,
     );
