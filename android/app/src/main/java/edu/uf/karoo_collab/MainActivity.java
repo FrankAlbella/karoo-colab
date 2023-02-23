910229ea70fd7a9b857580b53068305bc24684e1
JaimeToranzo <58479770+JaimeToranzo@users.noreply.github.com>
2023-02-23 16:49:04 -0500

    Fixed platform channel errors

--- android/app/src/main/java/edu/uf/karoo_collab/MainActivity.java
+++ android/app/src/main/java/edu/uf/karoo_collab/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
 import android.os.Bundle;
 import android.content.ContextWrapper;
 import android.content.Intent;
@@ -16,47 +18,48 @@ import android.os.Build.VERSION_CODES;
 
 
 public class MainActivity extends FlutterActivity {
-  private static final String CHANNEL = "edu.uf.karoo_collab";
-  private static int pHR;
+    private static final String CHANNEL = "edu.uf.karoo_collab";
+    private static int pHR;
 
-  @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
-            .setMethodCallHandler(
-                    (call, result) -> {
-                      if (call.method.equals("getBatteryLevel")) {
-                        int batteryLevel = getBatteryLevel(call.argument("HR"));
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                        (call, result) -> {
+                            if (call.method.equals("getBatteryLevel")) {
+                                int batteryLevel = getBatteryLevel(call.argument("HR"));
 
-                        if (batteryLevel != -1) {
-                          result.success(batteryLevel);
-                        } else {
-                          result.error("UNAVAILABLE", "Battery level not available.", null);
+                                if (batteryLevel != -1) {
+                                    result.success(batteryLevel);
+                                } else {
+                                    result.error("UNAVAILABLE", "Battery level not available.", null);
+                                }
+                            } else {
+                                result.notImplemented();
+                            }
                         }
-                      } else {
-                        result.notImplemented();
-                      }
-                    }
-            );
-  }
-  private int getBatteryLevel(int HR) {
-    pHR = HR;
-    System.out.println(HR);
-    int batteryLevel = -1;
-    if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-      BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
-      batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
-    } else {
-      Intent intent = new ContextWrapper(getApplicationContext()).
-              registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-      batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
-              intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+                );
     }
+    private int getBatteryLevel(int HR) {
+        pHR = HR;
+        System.out.println(HR);
+        int batteryLevel = -1;
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+        } else {
+            Intent intent = new ContextWrapper(getApplicationContext()).
+                    registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+            batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
+                    intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        }
 
-    return HR;
-  }
-  public static int getHR()
-  {
-    System.out.println(pHR);
-    return pHR;
-  }
+        return HR;
+    }
+    public static int getHR()
+    {
+        System.out.println(pHR);
+        return pHR;
+    }
 }
