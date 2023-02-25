438c90633bc130b4ecb0c6e041029b8a1bbbeaad
FrankAlbella <FrankAlbella@gmail.com>
2023-02-24 19:50:10 -0500

    PairingPage: add partner stat listening

--- lib/PairingPage.dart
+++ lib/PairingPage.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:karoo_collab/BluetoothProvider.dart';
+import 'package:logging/logging.dart';
 import 'BluetoothDeviceListEntry.dart';
 import 'BluetoothManager.dart';
+import 'RiderData.dart';
 
 Widget _buildPopupDialog(BuildContext context) {
   return AlertDialog(
@@ -60,15 +64,102 @@ class _PairingPage extends State<PairingPage> {
   Stream<BluetoothDiscoveryResult>? discoveryStream;
   StreamSubscription<BluetoothDiscoveryResult>? discoveryStreamSubscription;
 
+  static const platform = MethodChannel('edu.uf.karoo_collab');
+
+  double myHR = 0;
+  double myPower = 0;
+  double mySpeed = 0;
+
   @override
   void initState() {
     super.initState();
   
     //startBluetoothServer();
-    BluetoothManager.instance.deviceDataStream.listen((dataMap) {
-      print('got data from a connection: $dataMap');
-    });
+    // BluetoothManager.instance.deviceDataStream.listen((dataMap) {
+    //   print();
+    // });
     //startScan();
+    startBluetoothListening();
+  }
+
+  void startBluetoothListening() {
+    double partnerHR = 0;
+    double partnerPower = 0;
+    double partnerSpeed = 0;
+
+    BluetoothManager.instance.deviceDataStream.listen((event) {
+      Logger.root.info('got data from a connection: $event');
+      final map = event.values.first;
+
+      for(final key in map.keys) {
+        switch(key) {
+          case "heartRate":
+            partnerHR = double.parse(map[key] ?? "-1");
+            try {
+              platform.invokeListMethod('setPartnerHR', {"hr": partnerHR});
+              Logger.root.info('Partner HR set to $partnerHR');
+            } on PlatformException catch (e) {
+              Logger.root.severe('Failed to set partner HR: $e');
+            }
+            break;
+          case "power":
+            partnerPower = double.parse(map[key] ?? "-1");
+            try {
+              platform.invokeListMethod('setPartnerPower', {"power": partnerPower});
+              Logger.root.info('Partner power set to $partnerPower');
+            } on PlatformException catch (e) {
+              Logger.root.severe('Failed to set partner power: $e');
+            }
+            break;
+          case "speed":
+            partnerSpeed = double.parse(map[key] ?? "-1");
+            try {
+              platform.invokeListMethod('setPartnerSpeed', {"speed": partnerSpeed});
+              Logger.root.info('Partner power set to $partnerSpeed');
+            } on PlatformException catch (e) {
+              Logger.root.severe('Failed to set partner speed: $e');
+            }
+            break;
+          default:
+            Logger.root.warning('Unknown map key received: $key');
+        }
+      }
+    });
+
+    final streamController = StreamController<RiderData>();
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      _updateMyHR();
+      _updateMyPower();
+
+      final RiderData data = RiderData();
+      data.heartRate = myHR;
+      data.power = myPower;
+
+      streamController.add(data);
+
+    });
+
+    streamController.stream.listen((event) {
+      BluetoothManager.instance.broadcastString("heartRate:${event.heartRate}");
+      BluetoothManager.instance.broadcastString("power:${event.power}");
+      //BluetoothManager.instance.broadcastString("speed:${event.speed}");
+    });
+  }
+
+  Future<void> _updateMyHR() async{
+    try {
+      myHR = await platform.invokeMethod('getMyHR');
+    } on PlatformException catch (e) {
+      Logger.root.severe('Failed to get partner data from Stream: $e');
+    }
+  }
+
+  Future<void> _updateMyPower() async{
+    try {
+      myPower = await platform.invokeMethod('getMyPower');
+    } on PlatformException catch (e) {
+      Logger.root.severe('Failed to get partner data from Stream: $e');
+    }
   }
 
   //make the device discoverable and also
@@ -104,7 +195,6 @@ class _PairingPage extends State<PairingPage> {
           rssi: event.rssi,
           onTap: () {
             BluetoothManager.instance.connectToDevice(event.device);
-            
           },
         );
         if(deviceName.contains("Karoo")) {
