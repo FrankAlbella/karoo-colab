32d1dda09cc1690be7f9b50d3a47749937e6ee32
FrankAlbella <frankalbella@gmail.com>
2023-03-23 14:22:59 -0400

    Add logger.dart

---
+++ lib/logging/logger.dart
@@ -0,0 +1,20 @@
+class Logger {
+
+}
+
+class LoggerConstants {
+  static const EVENT_APP_LAUNCHED = 0;
+  static const EVENT_APP_CLOSED = 1;
+  static const EVENT_BUTTON_PRESSED = 2;
+  static const EVENT_PAGE_NAVIGATE = 3;
+  static const EVENT_SETTING_CHANGED = 4;
+  static const EVENT_WORKOUT_STARTED = 5;
+  static const EVENT_WORKOUT_ENDED = 6;
+  static const EVENT_WORKOUT_PAUSED = 7;
+  static const EVENT_WORKOUT_UNPAUSED = 8;
+  static const EVENT_PARTNER_CONNECT = 9;
+  static const EVENT_PARTNER_DISCONNECT = 10;
+  static const EVENT_BLUET00TH_INIT = 11;
+  static const EVENT_BLUET00TH_CONNECT = 12;
+  static const EVENT_BLUET00TH_DISCONNECT = 13;
+}
