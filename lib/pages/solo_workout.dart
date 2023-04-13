32f5de2838ba543e62bb122016343a33e0426bef
JaimeToranzo <58479770+JaimeToranzo@users.noreply.github.com>
2023-04-13 11:40:29 -0400

    added workout play/pause/end framework

--- lib/pages/solo_workout.dart
+++ lib/pages/solo_workout.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:wakelock/wakelock.dart';
-
 import '../ble_sensor_device.dart';
 import '../bluetooth_manager.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -27,19 +26,21 @@ class SoloWorkout extends StatefulWidget {
 }
 
 class _SoloWorkout extends State<SoloWorkout> {
-
   static int myHR = 0;
   int myPower = 0;
   int myCadence = 0;
-  int mySpeed = 0;
+  double mySpeed = 0;
   String _name = "";
   String _HR = "";
   String _FTP = "";
   Duration duration = Duration();
   Timer? timer;
   int distance = 0;
-  bool pauseWorkout = true;
-    bool stopWorkout = false;
+  bool pauseWorkout = false;
+  bool stopWorkout = false;
+  Position? _initialPosition;
+  Position? _currentPosition;
+  late StreamSubscription<Position> _positionStreamSubscription;
 
   final RiderData data = RiderData();
 
@@ -73,23 +74,19 @@ class _SoloWorkout extends State<SoloWorkout> {
     int time = data[11] << 8;
     time += data[10];
     double timeDouble = time.toDouble();
-    timeDouble *= 1/2048;
+    timeDouble *= 1 / 2048;
     return (1 / timeDouble) * 60.0;
-  }
-
-  void startTimer() {
-    timer = Timer.periodic(const Duration(seconds: 1), (_) => addTime());
   }
 
   void addTime() {
     setState(() {
       final seconds = duration.inSeconds + 1;
       duration = Duration(seconds: seconds);
-
-      //Testing purposes for peers
-      //BluetoothManager.instance.broadcastString('0: ${rng.nextInt(200)}');
-      //BluetoothManager.instance.broadcastString('1: ${150}');
     });
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(const Duration(seconds: 1), (_) => addTime());
   }
 
   void startSensorListening() {
@@ -97,25 +94,24 @@ class _SoloWorkout extends State<SoloWorkout> {
       for (BleSensorDevice device in widget.deviceList!) {
         if (device.type == 'HR') {
           debugPrint("Device sub: ${device.deviceId}");
-          subscribeStreamHR = widget.flutterReactiveBle.subscribeToCharacteristic(
-              QualifiedCharacteristic(
+          subscribeStreamHR = widget.flutterReactiveBle
+              .subscribeToCharacteristic(QualifiedCharacteristic(
                   characteristicId: device.characteristicId,
                   serviceId: device.serviceId,
-                  deviceId: device.deviceId
-              )).listen((event) {
+                  deviceId: device.deviceId))
+              .listen((event) {
             setState(() {
               myHR = event[1];
             });
           });
-        }
-        else if (device.type == 'POWER') {
+        } else if (device.type == 'POWER') {
           debugPrint("Device sub: ${device.deviceId}");
-          subscribeStreamHR = widget.flutterReactiveBle.subscribeToCharacteristic(
-              QualifiedCharacteristic(
+          subscribeStreamHR = widget.flutterReactiveBle
+              .subscribeToCharacteristic(QualifiedCharacteristic(
                   characteristicId: device.characteristicId,
                   serviceId: device.serviceId,
-                  deviceId: device.deviceId
-              )).listen((event) {
+                  deviceId: device.deviceId))
+              .listen((event) {
             setState(() {
               myPower = _readPower(event);
               myCadence = _readCadence(event).toInt();
@@ -132,80 +128,163 @@ class _SoloWorkout extends State<SoloWorkout> {
     startSensorListening();
     Wakelock.enable();
     _loadSettings();
+    startTimer();
   }
-  
 
   @override
   Widget build(BuildContext context) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    String? hours, minutes, seconds;
+    hours = twoDigits(duration.inHours.remainder(60));
+    minutes = twoDigits(duration.inMinutes.remainder(60));
+    seconds = twoDigits(duration.inSeconds.remainder(60));
     return Scaffold(
       backgroundColor: Colors.black26,
-      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
-      floatingActionButton: SizedBox(
-        child: FloatingActionButton(
+      floatingActionButton:
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        FloatingActionButton(
+          heroTag: "playpause",
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-          child: const Icon(Icons.arrow_back_rounded),
           onPressed: () {
-            ExerciseLogger.instance?.logButtonPressed("BackButton");
-            ExerciseLogger.instance?.logPageNavigate("solo_workout", "home_page");
-            Wakelock.disable();
-            Navigator.pop(context);
+            setState(() {
+              pauseWorkout = !pauseWorkout;
+              if (pauseWorkout) //PLAY/PAUSE WORKOUT!
+              {
+                timer?.cancel();
+              } else {
+                startTimer();
+              }
+            });
           },
+          child: Icon(pauseWorkout ? Icons.play_arrow : Icons.pause),
         ),
-      ),
+        Visibility(
+          visible: pauseWorkout == true,
+          child: FloatingActionButton(
+            heroTag: "endride",
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+            child: const Icon(Icons.delete),
+            onPressed: () {
+              //END WORKOUT!
+              stopWorkout = true;
+            },
+          ),
+        )
+      ]),
       body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-        Row(
-          children: const <Widget>[
-            Text("TIMER: ", style: TextStyle(fontSize: 25, color: Colors.white)),
-            Spacer(),
-          ],
-        ),
-        Row(
-          children: [
-            SizedBox.square(
-                dimension: 120,
-                child: Column(children: [
-                  Text(
-                  "$_name\'s HR:",
-                  style: const TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600),
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                    height: 80,
+                    width: MediaQuery.of(context).size.width/3,
+                  child: RichText(
+                    text: TextSpan(
+                      text: '\n\t\tDuration',
+                      style: const TextStyle(fontSize: 15, color: Colors.white),
+                      children: [
+                        TextSpan(
+                          text: '$minutes:$seconds',
+                          style: const TextStyle(fontSize: 25, color: Colors.white),
+                        ),
+                      ],
+                    ),
                   ),
-                  Text(
-                  "$myHR",
-                  style: const TextStyle(
-                      fontSize: 50,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600),
                 ),
-                ],)
-                ),
-            SizedBox.square(
-                dimension: 120,
-                child: Column(children: [
-                  Text(
-                  "$_name\'s Power:",
-                  style: const TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600),
-                  ),
-                  Text(
-                  "$myPower",
-                  style: const TextStyle(
-                      fontSize: 50,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600),
-                ),
-                ],)
-                ),
-          ],
-        ),
-      ])),
+                SizedBox(
+                    height: 80,
+                    width: MediaQuery.of(context).size.width/3,
+                    child: Column(
+                      children: [
+                        const Text(
+                          "Distance:",
+                          style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "$distance",
+                          style: const TextStyle(
+                              fontSize: 25,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    )),
+                SizedBox(
+                    height: 80,
+                    width: MediaQuery.of(context).size.width/3,
+                    child: Column(
+                      children: const [
+                        Text(
+                          "Speed:",
+                          style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "100",
+                          style: TextStyle(
+                              fontSize: 25,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    )),
+              ],
+            ),
+            Row(
+              children: [
+                SizedBox.square(
+                    dimension: 120,
+                    child: Column(
+                      children: [
+                        Text(
+                          "$_name\'s HR:",
+                          style: const TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "$myHR",
+                          style: const TextStyle(
+                              fontSize: 50,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    )),
+                SizedBox.square(
+                    dimension: 120,
+                    child: Column(
+                      children: [
+                        Text(
+                          "$_name\'s Power:",
+                          style: const TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "$myPower",
+                          style: const TextStyle(
+                              fontSize: 50,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    )),
+              ],
+            ),
+          ])),
     );
   }
 }
