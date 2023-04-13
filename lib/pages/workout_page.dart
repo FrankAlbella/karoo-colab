80c23f1097054282cb309957c60d20bd1a9e175c
FrankAlbella <frankalbella@gmail.com>
2023-04-13 18:11:01 -0400

    WorkoutPage: add setState to entire peer map parsing

--- lib/pages/workout_page.dart
+++ lib/pages/workout_page.dart
@@ -14,7 +14,8 @@ class WorkoutPage extends StatefulWidget {
   final FlutterReactiveBle flutterReactiveBle;
   final List<BleSensorDevice>? deviceList;
   final String title;
-  const WorkoutPage({super.key,
+  const WorkoutPage({
+    super.key,
     required this.flutterReactiveBle,
     required this.deviceList,
     required this.title,
@@ -27,7 +28,6 @@ class WorkoutPage extends StatefulWidget {
 class _WorkoutPage extends State<WorkoutPage> {
   Stream<BluetoothDiscoveryResult>? discoveryStream;
   StreamSubscription<BluetoothDiscoveryResult>? discoveryStreamSubscription;
-
 
   static int myHR = 0;
   int myPower = 0;
@@ -65,7 +65,7 @@ class _WorkoutPage extends State<WorkoutPage> {
     int time = data[11] << 8;
     time += data[10];
     double timeDouble = time.toDouble();
-    timeDouble *= 1/2048;
+    timeDouble *= 1 / 2048;
     return (1 / timeDouble) * 60.0;
   }
 
@@ -83,7 +83,7 @@ class _WorkoutPage extends State<WorkoutPage> {
     Wakelock.enable();
   }
 
-Future<void> _loadSettings() async {
+  Future<void> _loadSettings() async {
     final prefs = await SharedPreferences.getInstance();
     setState(() {
       _name = (prefs.getString('name') ?? "Name").substring(0, 4);
@@ -103,49 +103,48 @@ Future<void> _loadSettings() async {
     if (widget.deviceList != null) {
       debugPrint("MAYBE GOTTEM?");
       for (BleSensorDevice device in widget.deviceList!) {
-          debugPrint("we Gottem");
-          if (device.type == 'HR') {
-            debugPrint("Device sub: ${device.deviceId}");
-            subscribeStreamHR = widget.flutterReactiveBle.subscribeToCharacteristic(
-              QualifiedCharacteristic(
+        debugPrint("we Gottem");
+        if (device.type == 'HR') {
+          debugPrint("Device sub: ${device.deviceId}");
+          subscribeStreamHR = widget.flutterReactiveBle
+              .subscribeToCharacteristic(QualifiedCharacteristic(
                   characteristicId: device.characteristicId,
                   serviceId: device.serviceId,
-                  deviceId: device.deviceId
-              )).listen((event) {
-              setState(() {
-                // Update UI.
-                myHR = event[1];
-                // Broadcast heart rate to partner.
-                BluetoothManager.instance.broadcastString('heartRate:$myHR');
-                debugPrint("Broadcast string: heartRate:$myHR");
-                // Log heart rate.
-                //widget.logger.workout.logHeartRate(event[1]);
-              });
+                  deviceId: device.deviceId))
+              .listen((event) {
+            setState(() {
+              // Update UI.
+              myHR = event[1];
+              // Broadcast heart rate to partner.
+              BluetoothManager.instance.broadcastString('heartRate:$myHR');
+              debugPrint("Broadcast string: heartRate:$myHR");
+              // Log heart rate.
+              //widget.logger.workout.logHeartRate(event[1]);
             });
-          }
-          else if (device.type == 'POWER') {
-            debugPrint("Device sub: ${device.deviceId}");
-            subscribeStreamHR = widget.flutterReactiveBle.subscribeToCharacteristic(
-              QualifiedCharacteristic(
+          });
+        } else if (device.type == 'POWER') {
+          debugPrint("Device sub: ${device.deviceId}");
+          subscribeStreamHR = widget.flutterReactiveBle
+              .subscribeToCharacteristic(QualifiedCharacteristic(
                   characteristicId: device.characteristicId,
                   serviceId: device.serviceId,
-                  deviceId: device.deviceId
-              )).listen((event) {
-              setState(() {
-                // Update UI.
-                myPower = _readPower(event);
-                //myCadence = _readCadence(event).toInt();
-                // Broadcast power and cadence to partner.
-                BluetoothManager.instance.broadcastString('power:$myPower');
-                debugPrint("Broadcast string: power:$myPower");
-                //BluetoothManager.instance.broadcastString('cadence:$myCadence');
-                //debugPrint("Broadcast string: cadence:$myCadence");
-                // Log heart rate.
-                //widget.logger.workout.logHeartRate(event[1]);
-              });
+                  deviceId: device.deviceId))
+              .listen((event) {
+            setState(() {
+              // Update UI.
+              myPower = _readPower(event);
+              //myCadence = _readCadence(event).toInt();
+              // Broadcast power and cadence to partner.
+              BluetoothManager.instance.broadcastString('power:$myPower');
+              debugPrint("Broadcast string: power:$myPower");
+              //BluetoothManager.instance.broadcastString('cadence:$myCadence');
+              //debugPrint("Broadcast string: cadence:$myCadence");
+              // Log heart rate.
+              //widget.logger.workout.logHeartRate(event[1]);
             });
-          }
+          });
         }
+      }
     }
   }
 
@@ -153,48 +152,42 @@ Future<void> _loadSettings() async {
     BluetoothManager.instance.deviceDataStream.listen((event) {
       Logger.root.info('got data from a connection: $event');
       final map = event.values.first;
-      for(final key in map.keys) {
-        switch(key) {
-          case "heartRate":
-            debugPrint("we Gottem");
-            setState(() {
+      setState(() {
+        for (final key in map.keys) {
+          switch (key) {
+            case "heartRate":
               partnerHR = int.parse(map[key] ?? "-1");
-            });
-            Logger.root.info('Set partner HR: $partnerHR');
-            break;
-          case "power":
-            setState(() {
+              Logger.root.info('Set partner HR: $partnerHR');
+              break;
+            case "power":
               partnerPower = int.parse(map[key] ?? "-1");
-            });
-            Logger.root.info('Set partner power: $partnerPower');
-            break;
-          case "cadence":
-            setState(() {
+              Logger.root.info('Set partner power: $partnerPower');
+              break;
+            case "cadence":
               partnerCadence = int.parse(map[key] ?? "-1");
-            });
-            Logger.root.info('Set partner cadence: $partnerCadence');
-            break;
-          case "speed":
-            setState(() {
+              Logger.root.info('Set partner cadence: $partnerCadence');
+              break;
+            case "speed":
               partnerSpeed = int.parse(map[key] ?? "-1");
-            });
-            Logger.root.info('Set partner speed: $partnerSpeed');
-            break;
-          default:
-            Logger.root.warning('Unknown map key received: $key');
-        }}
+              Logger.root.info('Set partner speed: $partnerSpeed');
+              break;
+            default:
+              Logger.root.warning('Unknown map key received: $key');
+          }
+        }
+      });
     });
   }
 
   @override
   void dispose() {
-    peerSubscription = BluetoothManager.instance.deviceDataStream.listen((event) {});
+    peerSubscription =
+        BluetoothManager.instance.deviceDataStream.listen((event) {});
     if (subscribeStreamHR != null) {
       subscribeStreamHR?.cancel();
     }
     super.dispose();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -209,99 +202,104 @@ Future<void> _loadSettings() async {
       backgroundColor: Colors.black26,
       body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-        Row(
-          children: const <Widget>[
-            Text("TIMER: ", style: TextStyle(fontSize: 25, color: Colors.white)),
-            Spacer(),
-          ],
-        ),
-        Row(
-          children: [
-            SizedBox.square(
-                dimension: 120,
-                child: Column(children: [
-                  Text(
-                  "$_name\'s HR:",
-                  style: const TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600),
-                  ),
-                  Text(
-                  "$myHR",
-                  style: const TextStyle(
-                      fontSize: 50,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600),
-                ),
-                ],)
-                ),
-            SizedBox.square(
-                dimension: 120,
-                child: Column(children: [
-                  Text(
-                  "$_name\'s Power:",
-                  style: const TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600),
-                  ),
-                  Text(
-                  "$myPower",
-                  style: const TextStyle(
-                      fontSize: 50,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600),
-                ),
-                ],)
-                ),
-          ],
-        ),
-        Row(
-          children: [
-            SizedBox.square(
-                dimension: 120,
-                child: Column(children: [
-                  Text(
-                  "Partn's HR:",
-                  style: const TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600),
-                  ),
-                  Text(
-                  "$partnerHR",
-                  style: const TextStyle(
-                      fontSize: 50,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600),
-                ),
-                ],)
-                ),
-            SizedBox.square(
-                dimension: 120,
-                child: Column(children: [
-                  Text(
-                  "Partn's Power:",
-                  style: const TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600),
-                  ),
-                  Text(
-                  "$partnerPower",
-                  style: const TextStyle(
-                      fontSize: 50,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600),
-                ),
-                ],)
-                ),
-          ],
-        ),
-      ])),
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+            Row(
+              children: const <Widget>[
+                Text("TIMER: ",
+                    style: TextStyle(fontSize: 25, color: Colors.white)),
+                Spacer(),
+              ],
+            ),
+            Row(
+              children: [
+                SizedBox.square(
+                    dimension: 120,
+                    child: Column(
+                      children: [
+                        Text(
+                          "$_name\'s HR:",
+                          style: const TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "$myHR",
+                          style: const TextStyle(
+                              fontSize: 50,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    )),
+                SizedBox.square(
+                    dimension: 120,
+                    child: Column(
+                      children: [
+                        Text(
+                          "$_name\'s Power:",
+                          style: const TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "$myPower",
+                          style: const TextStyle(
+                              fontSize: 50,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    )),
+              ],
+            ),
+            Row(
+              children: [
+                SizedBox.square(
+                    dimension: 120,
+                    child: Column(
+                      children: [
+                        Text(
+                          "Partn's HR:",
+                          style: const TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "$partnerHR",
+                          style: const TextStyle(
+                              fontSize: 50,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    )),
+                SizedBox.square(
+                    dimension: 120,
+                    child: Column(
+                      children: [
+                        Text(
+                          "Partn's Power:",
+                          style: const TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "$partnerPower",
+                          style: const TextStyle(
+                              fontSize: 50,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    )),
+              ],
+            ),
+          ])),
       persistentFooterButtons: [
         IconButton(
           icon: const Icon(Icons.arrow_back_rounded),
